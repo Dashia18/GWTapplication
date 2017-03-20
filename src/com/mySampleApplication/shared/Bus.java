7078491a3beda589b55a632db0b7c4e3ce477242
Dashia <das-bobkova@yandex.ru>
2017-03-20 23:48:36 +0300

    Bus type for one bus record in the bus Timeuable

---
+++ src/com/mySampleApplication/shared/Bus.java
@@ -0,0 +1,56 @@
+package com.mySampleApplication.shared;
+
+import java.io.Serializable;
+
+/**
+ * Created by Daria Serebryakova on 20.03.2017.
+ */
+public class Bus implements Serializable{
+    //private + set/get
+    private  String number;
+    private  String beginstop;
+    private  String endstop;
+    private  String timeofsvobodasq;
+
+    public Bus() {
+    }
+
+    public Bus(String number, String beginstop, String endstop, String timeofsvobodasq) {
+        this.number = number;
+        this.beginstop = beginstop;
+        this.endstop = endstop;
+        this.timeofsvobodasq = timeofsvobodasq;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getBeginstop() {
+        return beginstop;
+    }
+
+    public String getEndstop() {
+        return endstop;
+    }
+
+    public String getTimeofsvobodasq() {
+        return timeofsvobodasq;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setBeginstop(String beginstop) {
+        this.beginstop = beginstop;
+    }
+
+    public void setEndstop(String endstop) {
+        this.endstop = endstop;
+    }
+
+    public void setTimeofsvobodasq(String timeofsvobodasq) {
+        this.timeofsvobodasq = timeofsvobodasq;
+    }
+}
