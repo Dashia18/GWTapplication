cf2f73a9fb2d9f82fdb58113c58d281f4efe502a
Dashia <das-bobkova@yandex.ru>
2017-03-21 14:57:32 +0300

    little chanchings

--- src/com/mySampleApplication/client/MySampleApplicationService.java
+++ src/com/mySampleApplication/client/MySampleApplicationService.java
@@ -1,9 +1,10 @@
 package com.mySampleApplication.client;
 
-import com.mySampleApplication.shared.Bus;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.mySampleApplication.shared.Bus;
 
 import java.util.List;
 
